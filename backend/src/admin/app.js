44c1457e0effac2af7ce840aeef75e6662cd4a5a
Raul Nicolas <dtower79@gmail.com>
2026-01-10 21:51:38 +0100

    Desactivar tours en frontend app.js

---
+++ backend/src/admin/app.js
@@ -0,0 +1,14 @@
+export default {
+  config: {
+    locales: [
+      // 'es',
+      // 'fr',
+    ],
+    // ESTA ES LA LÍNEA MÁGICA QUE ARREGLA EL ERROR VISUAL
+    tutorials: false,
+    notifications: { releases: false },
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
