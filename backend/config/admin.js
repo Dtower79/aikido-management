07bbb24502159c191f56ab379aae10f096e425d6
Raul Nicolas <dtower79@gmail.com>
2026-02-10 00:01:21 +0100

    Uptime Robot

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
+
   transfer: {
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
@@ -17,4 +18,5 @@ module.exports = ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+  url: env('STRAPI_ADMIN_BACKEND_URL', '/admin'),
 });
