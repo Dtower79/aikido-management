a4fd883b0f9407fec72eee82c7c843649537a29c
Raul Nicolas <dtower79@gmail.com>
2026-02-10 23:27:17 +0100

    Database structure synced - Final attempt to stabilize UI

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -2,7 +2,6 @@ module.exports = ({ env }) => ({
   auth: { secret: env('ADMIN_JWT_SECRET') },
   apiToken: { salt: env('API_TOKEN_SALT') },
   transfer: { token: { salt: env('TRANSFER_TOKEN_SALT') } },
-  // Forzamos ruta relativa para que no choque con la URL del servidor
-  url: '/admin', 
+  url: '/admin', // <--- ESTO ES VITAL
   tours: false,
 });
