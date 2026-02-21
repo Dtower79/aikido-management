72bfb7e21444035441e0d504c8738398f2bd4f0a
Raul Nicolas <dtower79@gmail.com>
2026-02-21 18:19:05 +0100

    Moviendo policy a carpeta global para visibilidad en UI

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arashi-v5.35-update';
+const CACHE_NAME = 'arashi-v5.41-update';
 
 // Activos críticos para que la UI cargue en el tatami sin internet
 const ASSETS_TO_CACHE = [
