23dd561a5c53f986d07be31b46d8c08df64bccf2
Raul Nicolas <dtower79@gmail.com>
2026-03-25 18:20:21 +0100

    VAMOH A VE PO FAVO

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arashi-v5.57-update';
+const CACHE_NAME = 'arashi-v5.58-update';
 
 // Activos críticos para que la UI cargue en el tatami sin internet
 const ASSETS_TO_CACHE = [
