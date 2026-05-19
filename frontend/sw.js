2e802e3ac05b306e7380985ab6eff1644f1f26fa
Raul Nicolas <dtower79@gmail.com>
2026-05-19 14:49:11 +0200

    TODOS LOS DOJOS CON CLASES VISIBLES EN INICIO

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arashi-v6.16-network-first'; // Cambiamos el nombre para forzar limpieza
+const CACHE_NAME = 'arashi-v6.20-network-first'; // Cambiamos el nombre para forzar limpieza
 
 const ASSETS_TO_CACHE =[
   './',
