5fca1134192f866e9b918efd3761d94a83ca46a6
Raul Nicolas <dtower79@gmail.com>
2026-05-19 15:52:52 +0200

    PARPADEOS

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arashi-v6.21-network-first'; // Cambiamos el nombre para forzar limpieza
+const CACHE_NAME = 'arashi-v6.22-network-first'; // Cambiamos el nombre para forzar limpieza
 
 const ASSETS_TO_CACHE =[
   './',
