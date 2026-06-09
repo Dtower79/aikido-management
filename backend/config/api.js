f48d286c61cc0ac175e1f4048ab6dd696921918c
Raul Nicolas <dtower79@gmail.com>
2026-06-10 01:26:50 +0200

    Aumentado el maxLimit de la API a 1000

--- backend/config/api.js
+++ backend/config/api.js
@@ -1,7 +1,10 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  responses: {
+    privateAttributes: ['_v', 'id', 'createdAt', 'updatedAt'],
+  },
   rest: {
     defaultLimit: 25,
-    maxLimit: 100,
+    maxLimit: 1000, // 🥋 AUMENTADO: Ampliamos el escudo de seguridad a 1000 registros por consulta
     withCount: true,
   },
-};
+});
